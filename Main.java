1bf291c7d3e3f23ba444d10ec20cb3cb08e3f713
DiogoGaldom <131724277+DiogoGaldom@users.noreply.github.com>
2023-06-11 23:01:05 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 9;
+
+        for (int i = 1; i <= n; ++i){
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
